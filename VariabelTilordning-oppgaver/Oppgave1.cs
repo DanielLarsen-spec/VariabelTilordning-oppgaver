9786958c77af05b6e61ef882f7891af2cbee8ca2
DanielLarsen-spec <larsen97daniel@gmail.com>
2024-11-19 19:33:43 +0100

    update

---
+++ VariabelTilordning-oppgaver/Oppgave1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariabelTilordning_oppgaver
+{
+    internal class Oppgave1
+    {
+        public void Run()
+        {
+            var input1 = Convert.ToInt32(Console.ReadLine());
+            var input2 = Convert.ToInt32(Console.ReadLine());
+
+            
+            Console.WriteLine(AddNumb(input1, input2));
+
+        }
+        private int AddNumb(int numb1, int numb2)
+        {
+            return numb1 + numb2;
+
+        }
+
+    }
+}
--- VariabelTilordning-oppgaver/Program.cs
+++ VariabelTilordning-oppgaver/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using VariabelTilordning_oppgaver;
+
+var oppgave = new Oppgave1();
+oppgave.Run();
+
